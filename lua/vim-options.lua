e20100c9f8ab0228538a058037ddab9e87dbf831
Tomas Sanchez Negrette <tomas.sancheznegrette@emeal.nttdata.com>
2024-03-29 12:24:24 -0300

    add lsp-signatures

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,7 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.cmd("set number")
+vim.cmd("set relativenumber")
 vim.cmd("set cursorline")
 vim.cmd("set termguicolors")
 vim.g.mapleader = " "
