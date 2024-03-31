5f0ea112aaa882e573acb8170bff441ac8fd6124
Tomas Sanchez Negrette <tomas.sancheznegrette@emeal.nttdata.com>
2024-03-30 22:39:56 -0300

    Add tmux-navigator

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -12,6 +12,7 @@ local Lsps = {
   "taplo",
   "tailwindcss",
   "lemminx",
+  "jdtls",
 }
 return {
   {
@@ -49,7 +50,8 @@ return {
       })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
     end,
   },
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,27 @@
+local ensure_installed = {
+  "astro",
+  "cmake",
+  "cpp",
+  "css",
+  "gitignore",
+  "go",
+  "graphql",
+  "http",
+  "java",
+  "scss",
+  "sql",
+  "typescript",
+  "tsx",
+}
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local tree_sitter = require("nvim-treesitter.configs")
-		tree_sitter.setup({
-			auto_install = true,
-			highlight = { enabled = true },
-			indent = { enabled = true },
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local tree_sitter = require("nvim-treesitter.configs")
+    tree_sitter.setup({
+      ensure_installed = ensure_installed,
+      highlight = { enabled = true },
+      indent = { enabled = true },
+    })
+  end,
 }
