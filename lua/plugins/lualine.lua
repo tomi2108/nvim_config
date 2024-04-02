0d498b52b55415e13058f2292801608dff237076
tomi2108 <tominegrette@hotmail.com>
2024-04-02 01:00:07 -0300

    Edit lualine theme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,37 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "onenord",
-			},
-		})
-	end,
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    local theme = require("lualine.themes.onenord")
+    local sections = { "a", "b" }
+
+    for _, letter in pairs(sections) do
+      theme.normal[letter].bg = "#2e3440"
+      theme.insert[letter].bg = "#2e3440"
+      theme.visual[letter].bg = "#2e3440"
+      theme.replace[letter].bg = "#2e3440"
+      theme.command[letter].bg = "#2e3440"
+    end
+
+    theme.normal.a.fg = theme.normal.b.fg
+    theme.insert.a.fg = theme.insert.b.fg
+    theme.visual.a.fg = theme.visual.b.fg
+    theme.replace.a.fg = theme.replace.b.fg
+    theme.command.a.fg = theme.command.b.fg
+
+    theme.normal.c.bg = "#2e3440"
+
+    require("lualine").setup({
+      options = {
+        theme = theme,
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { { "filename", path = 1 } },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+    })
+  end,
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,6 +6,7 @@ vim.cmd("set relativenumber")
 vim.cmd("set number")
 vim.cmd("set cursorline")
 vim.cmd("set termguicolors")
+vim.cmd("set scrolloff")
 vim.g.mapleader = " "
 
 -- format on save
@@ -13,11 +14,11 @@ local autocmd = vim.api.nvim_create_autocmd
 local augroupf = vim.api.nvim_create_augroup
 augroupf("__formatter__", { clear = true })
 autocmd("BufWritePost", {
-  group = "__formatter__",
-  command = ":FormatWrite",
+	group = "__formatter__",
+	command = ":FormatWrite",
 })
 
 -- show diagnostics in insert mode
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-  update_in_insert = true,
+	update_in_insert = true,
 })
