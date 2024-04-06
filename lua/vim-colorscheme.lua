9d6d8dbf1e2722ce21a5198b52cb1f5aca48196c
tomi2108 <tominegrette@hotmail.com>
2024-04-06 11:39:02 -0300

    Update colorscheme

--- lua/vim-colorscheme.lua
+++ lua/vim-colorscheme.lua
@@ -1,14 +1,15 @@
 local custom_colors = require("addons.everforest-palette")
 require("everforest").setup({
 	on_highlights = function(hl, palette)
-		hl.FloatBorder = { fg = palette.grey1, bg = palette.bg0 }
-		hl.NeoTreeFloatNormal = { bg = palette.bg0, fg = palette.grey1 }
-		hl.NeoTreeFloatTitle = { bg = palette.bg0, fg = palette.grey1 }
-		hl.NeoTreeFloatBorder = { fg = palette.grey1, bg = palette.bg0 }
-		hl.NeoTreeNormal = { bg = palette.bg0 }
-		hl.NeoTreeEndOfBuffer = { bg = palette.bg0, fg = palette.bg0 }
+		hl.FloatBorder = { fg = palette.grey1, bg = "none" }
+		hl.NeoTreeFloatNormal = { bg = "none", fg = palette.grey1 }
+		hl.NeoTreeFloatTitle = { bg = "none", fg = palette.grey1 }
+		hl.NeoTreeFloatBorder = { fg = palette.grey1, bg = "none" }
+		hl.NeoTreeNormal = { bg = "none" }
+		hl.NeoTreeEndOfBuffer = { bg = "none", fg = palette.bg0 }
 		hl.TSString = { fg = palette.orange, bg = palette.none }
 		hl.String = { fg = palette.orange, bg = palette.none }
+		hl.EndOfBuffer = { bg = "none", fg = palette.bg0 }
 	end,
 	colours_override = function(palette)
 		for k, v in pairs(custom_colors) do
