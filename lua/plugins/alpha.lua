6e41a68e70a1336b66f15f742b14ee64f49654d7
Tomas Sanchez Negrette <tomas.sancheznegrette@emeal.nttdata.com>
2024-04-19 15:58:25 -0300

    Change alpha unicode char

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,77 +1,73 @@
 return {
-	"goolord/alpha-nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-		"nvim-lua/plenary.nvim",
-	},
+  "goolord/alpha-nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "nvim-lua/plenary.nvim",
+  },
 
-	config = function()
-		local alpha = require("alpha")
-		local theta = require("alpha.themes.theta")
-		local cdir = vim.fn.getcwd()
-		local mru = theta.mru
-		local plugin_list = require("lazy").plugins()
-		local plugin_count = 0
-		for _ in pairs(plugin_list) do
-			plugin_count = plugin_count + 1
-		end
-		local header_val = {
-			[[                 *       +]],
-			[[           '                  |]],
-			[[       ()    .-.,="``"=.    - o -]],
-			[[             '=/_       \     |]],
-			[[          *   |  '=._    |]],
-			[[               \     `=./`,        ']],
-			[[            .   '=.__.=' `='      *]],
-			[[   +                         +]],
-			[[        O      *        '       .]],
-		}
-		local header = {
-			type = "text",
-			val = header_val,
-			opts = {
-				position = "center",
-				hl = "Type",
-			},
-		}
-		local section_mru = {
-			type = "group",
-			val = {
-				{
-					type = "text",
-					val = "Recent files",
-					opts = {
-						hl = "SpecialComment",
-						shrink_margin = false,
-						position = "center",
-					},
-				},
-				{ type = "padding", val = 1 },
-				{
-					type = "group",
-					val = function()
-						return { mru(0, cdir) }
-					end,
-					opts = { shrink_margin = false },
-				},
-			},
-		}
-		theta.config.layout = {
-			{ type = "padding", val = 2 },
-			header,
-			{ type = "padding", val = 2 },
-			section_mru,
-			{ type = "padding", val = 1 },
-			{
-				type = "text",
-				val = string.format("♍ %s Plugins installed  ♍", plugin_count),
-				opts = {
-					position = "center",
-					hl = "SpecialComment",
-					shrink_margin = false,
-				},
-			},
-		}
-		alpha.setup(theta.config)
-	end,
+  config = function()
+    local alpha = require("alpha")
+    local theta = require("alpha.themes.theta")
+    local cdir = vim.fn.getcwd()
+    local mru = theta.mru
+    local plugin_list = require("lazy").plugins()
+    local plugin_count = 0
+    for _ in pairs(plugin_list) do
+      plugin_count = plugin_count + 1
+    end
+    local header_val = {
+      [[                 *       +]],
+      [[           '                  |]],
+      [[       ()    .-.,="``"=.    - o -]],
+      [[             '=/_       \     |]],
+      [[          *   |  '=._    |]],
+      [[               \     `=./`,        ']],
+      [[            .   '=.__.=' `='      *]],
+      [[   +                         +]],
+      [[        O      *        '       .]],
+    }
+    local header = {
+      type = "text",
+      val = header_val,
+      opts = {
+        position = "center",
+        hl = "Type",
+      },
+    }
+    local section_mru = {
+      type = "group",
+      val = {
+        {
+          type = "text",
+          val = "Recent files",
+          opts = {
+            hl = "SpecialComment",
+            shrink_margin = false,
+            position = "center",
+          },
+        },
+        { type = "padding", val = 1 },
+        {
+          type = "group",
+          val = function()
+            return { mru(0, cdir) }
+          end,
+          opts = { shrink_margin = false },
+        },
+      },
+    }
+    theta.config.layout = {
+      { type = "padding", val = 2 },
+      header,
+      { type = "padding", val = 2 },
+      section_mru,
+      { type = "padding", val = 1 },
+      {
+        type = "text",
+        val = string.format("✧ %s Plugins installed ✧", plugin_count),
+        opts = { position = "center", hl = "SpecialComment", shrink_margin = false },
+      },
+    }
+    alpha.setup(theta.config)
+  end,
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,72 +1,72 @@
 local Lsps = {
-  "lua_ls",
-  "clangd",
-  "cssls",
-  "eslint",
-  "html",
-  "jsonls",
-  "tsserver",
-  "marksman",
-  -- "sonarlint-language-server",
-  "somesass_ls",
-  "taplo",
-  "gopls",
-  "tailwindcss",
-  "lemminx",
-  "jdtls",
+	"lua_ls",
+	"clangd",
+	"cssls",
+	"eslint",
+	"html",
+	"jsonls",
+	"tsserver",
+	"marksman",
+	-- "sonarlint-language-server",
+	"somesass_ls",
+	"taplo",
+	-- "gopls",
+	"tailwindcss",
+	"lemminx",
+	"jdtls",
 }
 local custom_setup = {
-  "clangd",
+	"clangd",
 }
 
 return {
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = Lsps,
-    },
-    config = function()
-      require("mason").setup({})
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = Lsps,
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = require("addons.typescript"),
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lsp = require("lspconfig")
-      for _, v in pairs(Lsps) do
-        if custom_setup[v] == nil then
-          lsp[v].setup({
-            capabilities = capabilities,
-          })
-        end
-      end
-      lsp.clangd.setup({
-        capabilities = capabilities,
-        cmd = {
-          "clangd",
-          "--offset-encoding=utf-16",
-        },
-      })
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = Lsps,
+		},
+		config = function()
+			require("mason").setup({})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = Lsps,
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = require("addons.typescript"),
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lsp = require("lspconfig")
+			for _, v in pairs(Lsps) do
+				if custom_setup[v] == nil then
+					lsp[v].setup({
+						capabilities = capabilities,
+					})
+				end
+			end
+			lsp.clangd.setup({
+				capabilities = capabilities,
+				cmd = {
+					"clangd",
+					"--offset-encoding=utf-16",
+				},
+			})
 
-      -- Borders on hover
-      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-        border = "rounded",
-      })
+			-- Borders on hover
+			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+				border = "rounded",
+			})
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", require("telescope.builtin").lsp_references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", require("telescope.builtin").lsp_references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
