158d57eec7f3ef03306a2c029afdc9adad98eb51
tomi2108 <tominegrette@hotmail.com>
2024-04-20 00:56:48 -0300

    Add hex editor

--- lua/plugins/error-lens.lua
+++ lua/plugins/error-lens.lua
@@ -1,20 +1,20 @@
-local palette = require("addons.everforest-palette")
+-- local palette = require("addons.everforest-palette")
 return {
-  "chikko80/error-lens.nvim",
-  event = "BufRead",
-  dependencies = {
-    "nvim-telescope/telescope.nvim",
-  },
-  opts = {
-    colors = {
-      error_fg = palette.red,
-      error_bg = palette.bg0,
-      warn_fg = palette.yellow,
-      warn_bg = palette.bg0,
-      info_fg = palette.blue,
-      info_bg = palette.bg0,
-      hint_fg = palette.green,
-      hint_bg = palette.bg0,
-    },
-  },
+	-- "chikko80/error-lens.nvim",
+	-- event = "BufRead",
+	-- dependencies = {
+	--   "nvim-telescope/telescope.nvim",
+	-- },
+	-- opts = {
+	--   colors = {
+	--     error_fg = palette.red,
+	--     error_bg = palette.bg0,
+	--     warn_fg = palette.yellow,
+	--     warn_bg = palette.bg0,
+	--     info_fg = palette.blue,
+	--     info_bg = palette.bg0,
+	--     hint_fg = palette.green,
+	--     hint_bg = palette.bg0,
+	--   },
+	-- },
 }
