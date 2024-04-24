2d4894a9e3ca54c6bbb609bff0302e2510a63374
Tomas Sanchez Negrette <tomas.sancheznegrette@emeal.nttdata.com>
2024-04-23 23:07:57 -0300

    Fix treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,48 +1,48 @@
 local ensure_installed = {
-	"astro",
-	"cmake",
-	"cpp",
-	"css",
-	"gitignore",
-	"go",
-	"graphql",
-	"http",
-	"java",
-	"scss",
-	"sql",
-	"typescript",
-	"tsx",
-	"vim",
-	"regex",
-	"lua",
-	"bash",
-	"markdown",
-	"markdown_inline",
+  "astro",
+  "cmake",
+  "cpp",
+  "css",
+  "gitignore",
+  "go",
+  "graphql",
+  "http",
+  "java",
+  "scss",
+  "sql",
+  "typescript",
+  "tsx",
+  "vim",
+  "regex",
+  "lua",
+  "bash",
+  "markdown",
+  "markdown_inline",
 }
 return {
-	{
+  {
 
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local tree_sitter = require("nvim-treesitter.configs")
-			tree_sitter.setup({
-				ensure_installed = ensure_installed,
-				highlight = { enabled = true },
-				indent = { enabled = true },
-			})
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter-context",
-		config = function()
-			require("treesitter-context").setup({
-				max_lines = 3,
-			})
-			vim.keymap.set("n", "[c", function()
-				require("treesitter-context").go_to_context(vim.v.count1)
-				vim.api.nvim_feedkeys("zz", "n", true)
-			end, { silent = true })
-		end,
-	},
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local tree_sitter = require("nvim-treesitter.configs")
+      tree_sitter.setup({
+        ensure_installed = ensure_installed,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    config = function()
+      require("treesitter-context").setup({
+        max_lines = 3,
+      })
+      vim.keymap.set("n", "[c", function()
+        require("treesitter-context").go_to_context(vim.v.count1)
+        vim.api.nvim_feedkeys("zz", "n", true)
+      end, { silent = true })
+    end,
+  },
 }
