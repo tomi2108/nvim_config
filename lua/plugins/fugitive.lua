fbd2a1e8cb949aad190d029c16ae25d92d5507ad
tomi2108 <tominegrette@hotmail.com>
2024-07-12 23:39:13 -0300

    Change <leader>gs to <leader>gg

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,7 +2,7 @@ return {
 	"tpope/vim-fugitive",
 	config = function()
 		vim.keymap.set("n", "<leader>gc", ":Git commit<CR>")
-		vim.keymap.set("n", "<leader>gs", ":Gedit :<CR>")
+		vim.keymap.set("n", "<leader>gg", ":Gedit :<CR>")
 		vim.keymap.set("n", "<leader>gp", ":Git pull<CR>")
 		vim.keymap.set("n", "<leader>gP", ":Git push<CR>")
 		vim.keymap.set("n", "<leader>gb", ":Telescope git_branches<CR>")
