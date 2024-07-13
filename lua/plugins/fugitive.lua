b80bc743b36fb4d3c968a8404d4bcdea9c502f95
tomi2108 <tominegrette@hotmail.com>
2024-07-13 01:35:03 -0300

    restore push and pull

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,6 +2,8 @@ return {
   "tpope/vim-fugitive",
   config = function()
     vim.keymap.set("n", "<C-g>", ":Gedit :<CR>")
+    vim.keymap.set("n", "<C-g><C-p>", ":Git pull<CR>")
+    vim.keymap.set("n", "<C-g><C-P>", ":Git push<CR>")
     vim.cmd("let g:nremap = {'=': '<TAB>'}");
   end,
 }
