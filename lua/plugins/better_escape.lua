78c40b93ca599aa7245bbcc8c53fafa536544217
tomi2108 <tominegrette@hotmail.com>
2024-07-29 20:27:54 -0300

    smaller timeout for better_escape

--- lua/plugins/better_escape.lua
+++ lua/plugins/better_escape.lua
@@ -1,6 +1,8 @@
 return {
   "max397574/better-escape.nvim",
   config = function()
-    require("better_escape").setup()
+    require("better_escape").setup({
+      timeout = 100,
+    })
   end,
 }
