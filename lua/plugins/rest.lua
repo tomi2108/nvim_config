4ce645b371e6697a0ecdf9dc04a09346e237383b
Tomas Sanchez Negrette <tomas.sancheznegrette@emeal.nttdata.com>
2024-08-05 18:53:14 -0300

    fix

---
+++ lua/plugins/rest.lua
@@ -0,0 +1,29 @@
+return {
+	{
+		"vhyrro/luarocks.nvim",
+		priority = 1000,
+		config = true,
+		opts = {
+			rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
+		},
+	},
+	{
+		"rest-nvim/rest.nvim",
+		ft = "http",
+		dependencies = { "luarocks.nvim" },
+		config = function()
+			vim.keymap.set("n", "<leader>r", ":Rest run<CR>")
+			require("rest-nvim").setup({
+				result = {
+					split = {
+						in_place = true,
+					},
+				},
+				highlight = {
+					enable = false,
+          timeout = 0
+				},
+			})
+		end,
+	},
+}
