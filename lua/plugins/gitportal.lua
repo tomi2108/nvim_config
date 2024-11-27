cd404e2a31e0f51b753d2bc2ea3749129e63eaa2
Tomas Sanchez Negrette <tomas.sancheznegrette@emeal.nttdata.com>
2024-11-27 01:27:13 -0300

    add gitportal copy to clipboard

--- lua/plugins/gitportal.lua
+++ lua/plugins/gitportal.lua
@@ -9,6 +9,8 @@ return {
 
     vim.keymap.set("n", "<leader>gp", function() gitportal.open_file_in_browser() end)
     vim.keymap.set("v", "<leader>gp", function() gitportal.open_file_in_browser() end)
-    vim.keymap.set('n', '<leader>ig', function() gitportal.open_file_in_neovim() end)
+    vim.keymap.set("n", '<leader>ig', function() gitportal.open_file_in_neovim() end)
+    vim.keymap.set("n", '<leader>gc', function() gitportal.copy_link_to_clipboard() end)
+    vim.keymap.set("v", '<leader>gc', function() gitportal.copy_link_to_clipboard() end)
   end
 }
